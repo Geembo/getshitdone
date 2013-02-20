7ddabbc2aeb3d91047fe90f17e521774077a571c
Angel Avgerinos <evaggelos.avgerinos@gmail.com>
2013-02-20 13:42:36 +0200

    Refactors user_spec to stop testing for password_confirmation

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Ellen Page"
+    email "elen@page.in"
+    password "inception"
+    password_confirmation "inception"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 describe User do
   before do
-    @user = User.new(email: "foo@getshitdone.com", password: "foobar", 
-                     password_confirmation: "foobar")
+    @user = User.new(email: "foo@getshitdone.com", password: "foobar")
   end
   
   subject { @user }
@@ -11,7 +10,6 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -51,22 +49,12 @@ describe User do
   end
 
   describe "when password is not present" do
-    before { @user.password = @user.password_confirmation = " " }
-    it { should_not be_valid }
-  end
-
-  describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
-    it { should_not be_valid }
-  end
-
-  describe "when password confirmation is nil" do
-    before { @user.password_confirmation = nil }
+    before { @user.password = " " }
     it { should_not be_valid }
   end
 
   describe "with password that's too short" do
-    before { @user.password = @user.password_confirmation = 'a' * 5 }
+    before { @user.password = 'a' * 5 }
     it { should_not be_valid }
   end
 
