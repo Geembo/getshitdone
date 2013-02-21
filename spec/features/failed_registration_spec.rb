e3caebce6a71b57fc5955bb05773e91eb40b8b36
Angel Avgerinos <evaggelos.avgerinos@gmail.com>
2013-02-21 13:32:19 +0200

    A little DRYer

--- spec/features/failed_registration_spec.rb
+++ spec/features/failed_registration_spec.rb
@@ -3,11 +3,10 @@ require 'spec_helper'
 feature "Failed registration" do
 
   before do
-    @user = FactoryGirl.create(:user)
+    visit register_path
   end
 
   scenario "fills in only mail" do
-    visit register_path
     fill_in "Email", with: "el@page.page"
     click_button "Register"
     page.should have_selector('h4', "Some errors occured")
@@ -15,7 +14,7 @@ feature "Failed registration" do
   end
 
   scenario "fills in with email of an existing user" do
-    visit register_path
+    @user = FactoryGirl.create(:user)
     fill_in "Email", with: @user.email
     fill_in "Password", with: "a"*15
     click_button "Register"
